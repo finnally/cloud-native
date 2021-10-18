439b06e5b82f17c4fce2c7d6af904e292102d4be
finnally <286882783@qq.com>
2021-10-18 16:21:42 +0800

    Update clean.sh

--- httpserver/clean.sh
+++ httpserver/clean.sh
@@ -3,6 +3,7 @@
 container_name=$(awk '{print $1}' /tmp/.info)
 image_name=$(awk '{print $2}' /tmp/.info)
 
-echo "remove container ${container_name} and image ${image_name}"
+echo -e "\033[1;32mremove container ${container_name} and image ${image_name}\033[1;0m"
+pid=$(docker inspect -f {{.State.Pid}} ${container_name} 2>/dev/null)
 kill $pid && sleep 2 && docker rm ${container_name}
 docker rmi ${image_name}
