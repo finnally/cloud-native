3c2d926b021dc4df545c92009ba88ca2d05c4b34
finally <286882783@qq.com>
2021-12-12 17:51:31 +0800

    add metrics

--- httpserver/main.go
+++ httpserver/main.go
@@ -13,13 +13,6 @@ import (
 	"time"
 )
 
-/*
-	1. 接收客户端request，并将request中带的header写入response header
-	2. 读取当前系统的环境变量中VERSION配置，并写入response header
-	3. Server端记录访问日志包括客户端IP，HTTP返回码，输出到Server端的标准输出
-	4. 当访问localhost/healthz时，应返回200
-*/
-
 type newResponseWriter struct {
 	http.ResponseWriter
 	statusCode int
